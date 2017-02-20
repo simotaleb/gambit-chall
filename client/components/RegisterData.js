c471175ef4a3b7a1bca763560f3ea8a853f6fa7e
simotaleb <simotaleb@gmail.com>
2017-02-20 20:46:16 +0200

    new feature on rown click

--- client/components/RegisterData.js
+++ client/components/RegisterData.js
@@ -6,8 +6,16 @@ export default class RegisterData extends React.Component{
     constructor(){
         super()
         this.state = {
-            rowData : undefined
-        }
+            rowData : undefined,
+            indice : 0
+        };
+    }
+    getIndex(evt){
+        this.setState({
+            indice: evt
+            //rowData: this.state.rowData[index]
+        });
+        this.refs.simpleDialog.show()
     }
     render(){
         let list = [];
@@ -19,7 +27,7 @@ export default class RegisterData extends React.Component{
             str = str.split(':');
             this.state.rowData = str;
             //console.log(this.state.rowData);
-            list.push(<tr key={index} className="w3-hover-green" onClick={()=>this.refs.simpleDialog.show()}>
+            list.push(<tr key={index} className="w3-hover-green" onClick={this.getIndex.bind(this,index)}>
                         <td>{str[0]}</td>
                         <td>{str[1]}</td>
                       </tr>);
@@ -29,10 +37,11 @@ export default class RegisterData extends React.Component{
         console.log('final state of index : ',index);
         this.state.rowData = list;
         //console.log(this.state.rowData);
+        let x = this.state.indice;
         return(
           <div className="w3-responsive">
             <table className="w3-table-all">
-                <thead>
+                <thead>~
                     <tr className="w3-red">
                         <th>Register</th>
                         <th>Value</th>
@@ -45,6 +54,18 @@ export default class RegisterData extends React.Component{
             <SkyLight ref='simpleDialog' title='Details'>
                 <div className="w3-display-middle">
                     <a className="w3-btn w3-padding-xxlarge"  href='https://gambitgroup.fi'>Take me to gambit</a>
+                    <table className="w3-table-all">
+                        <thead>
+                            <tr className="w3-red">
+                                <th>Register</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.rowData[x-1]}
+                        </tbody>
+                    </table>
+                    
                 </div>
                      
             </SkyLight>
